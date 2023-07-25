484b437c8adf6310ac016c6f23fa51bcf94ce758
PIYUJIN <yjyj020201@gmail.com>
2023-07-25 15:25:00 +0900

    [Feat] 회원가입 화면 전환 구현

--- app/src/main/java/com/test/board_project/LoginFragment.kt
+++ app/src/main/java/com/test/board_project/LoginFragment.kt
@@ -1,10 +1,17 @@
 package com.test.board_project
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.board_project.MainActivity.Companion.JOIN_FRAGMENT
+import com.test.board_project.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +24,31 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var mainActivity: MainActivity
+    lateinit var fragmentLoginBinding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentLoginBinding.run{
+            toolbarLogin.run {
+                title = "로그인"
+            }
+
+            buttonJoin.setOnClickListener {
+                mainActivity.replaceFragment(JOIN_FRAGMENT,true,null)
+            }
+        }
+
+        return fragmentLoginBinding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }
