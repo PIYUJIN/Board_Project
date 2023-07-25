3c7b94c805a678977f00b2372250921a05e353f2
PIYUJIN <yjyj020201@gmail.com>
2023-07-25 09:49:55 +0900

    [Feat] Splash Screen 구현

--- app/src/main/java/com/test/board_project/MainActivity.kt
+++ app/src/main/java/com/test/board_project/MainActivity.kt
@@ -2,12 +2,16 @@ package com.test.board_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
