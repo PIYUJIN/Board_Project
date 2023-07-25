101daa07986732dde797db003fd825d8c789ce0d
PIYUJIN <yjyj020201@gmail.com>
2023-07-25 16:07:57 +0900

    [Feat] back button & 가입 완료 버튼 화면 전환 기능 구현

--- app/src/main/java/com/test/board_project/AddUserInfoFragment.kt
+++ app/src/main/java/com/test/board_project/AddUserInfoFragment.kt
@@ -1,59 +1,51 @@
 package com.test.board_project
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.board_project.databinding.FragmentAddUserInfoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddUserInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddUserInfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var mainActivity: MainActivity
+    lateinit var fragmentAddUserInfoBinding: FragmentAddUserInfoBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user_info, container, false)
-    }
+        mainActivity = activity as MainActivity
+        fragmentAddUserInfoBinding = FragmentAddUserInfoBinding.inflate(inflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddUserInfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddUserInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        fragmentAddUserInfoBinding.run {
+            toolbarAddUserInfo.run {
+                title = "유저 정보"
+
+                setNavigationIcon(R.drawable.arrow_back_24px)
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    navigationIcon?.colorFilter = BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.JOIN_FRAGMENT)
                 }
             }
+
+            buttonCompleteJoin.setOnClickListener {
+                mainActivity.removeFragment(MainActivity.ADD_USER_INFO_FRAGMENT)
+                mainActivity.removeFragment(MainActivity.JOIN_FRAGMENT)
+            }
+        }
+        return fragmentAddUserInfoBinding.root
     }
 }
