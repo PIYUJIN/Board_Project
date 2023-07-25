d7a1be8f70518592d511fb3629eb4729535c6f95
PIYUJIN <yjyj020201@gmail.com>
2023-07-25 09:40:29 +0900

    [Create] 프로젝트 생성

---
+++ app/src/main/java/com/test/board_project/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.test.board_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
