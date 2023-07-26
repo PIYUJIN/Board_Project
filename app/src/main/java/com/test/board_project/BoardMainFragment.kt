dfd0d97ce0d06826ed38e4e97fcc6d9ced7729ef
PIYUJIN <yjyj020201@gmail.com>
2023-07-26 13:56:12 +0900

    [Feat] Modal navigation drawer 기능 설정

--- app/src/main/java/com/test/board_project/BoardMainFragment.kt
+++ app/src/main/java/com/test/board_project/BoardMainFragment.kt
@@ -1,59 +1,53 @@
 package com.test.board_project
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.board_project.databinding.FragmentBoardMainBinding
+import com.test.board_project.databinding.HeaderBoardMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BoardMainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BoardMainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var mainActivity: MainActivity
+    lateinit var fragmentBoardMainBinding: FragmentBoardMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_board_main, container, false)
-    }
+        mainActivity = activity as MainActivity
+        fragmentBoardMainBinding = FragmentBoardMainBinding.inflate(inflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BoardMainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BoardMainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        fragmentBoardMainBinding.run {
+            toolBarMain.run {
+                title = "게시판"
+
+                setNavigationIcon(R.drawable.menu_24px)
+                setNavigationOnClickListener{
+                    // navigation 보여주기
+                    drawerLayoutBoardMain.open()
                 }
             }
+
+            // DrawerView
+            navigationViewBoardMain.run{
+
+                // 헤더설정
+                val headerBoardMainBinding = HeaderBoardMainBinding.inflate(inflater)
+                headerBoardMainBinding.textViewHeaderBoardMainNickName.text  = "홍길동님"
+                addHeaderView(headerBoardMainBinding.root)
+            }
+        }
+
+        return fragmentBoardMainBinding.root
     }
+
 }
