45272d2b36c4748d0c69ca320c5d70380120c0c3
PIYUJIN <yjyj020201@gmail.com>
2023-07-28 03:13:25 +0900

    [Feat] 게시판 목록 RecyclerView Adapter 구현

--- app/src/main/java/com/test/board_project/PostListFragment.kt
+++ app/src/main/java/com/test/board_project/PostListFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.test.board_project.databinding.FragmentPostListBinding
+import com.test.board_project.databinding.RowPostListBinding
 
 class PostListFragment : Fragment() {
 
@@ -20,7 +24,91 @@ class PostListFragment : Fragment() {
         mainActivity = activity as MainActivity
         fragmentPostListBinding = FragmentPostListBinding.inflate(inflater)
 
+        fragmentPostListBinding.run{
+
+            recyclerViewPostListAll.run{
+                adapter = AllREcyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+
+            recyclerViewPostListResult.run{
+                adapter = ResultRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+
         return fragmentPostListBinding.root
+    }
+
+    // 모든 게시글 목록을 보여주는 리사이클러 뷰의 어뎁터
+    inner class AllREcyclerViewAdapter : RecyclerView.Adapter<AllREcyclerViewAdapter.AllViewHolder>(){
+        inner class AllViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+
+            val rowPostListSubject: TextView
+            val rowPostListNickName: TextView
+
+            init{
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = AllViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: AllViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
+    }
+
+
+    // 검색 결과 게시글 목록을 보여주는 리사이클러 뷰의 어뎁터
+    inner class ResultRecyclerViewAdapter : RecyclerView.Adapter<ResultRecyclerViewAdapter.ResultViewHolder>(){
+        inner class ResultViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+
+            val rowPostListSubject: TextView
+            val rowPostListNickName: TextView
+
+            init{
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = ResultViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: ResultViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
     }
 }
 
