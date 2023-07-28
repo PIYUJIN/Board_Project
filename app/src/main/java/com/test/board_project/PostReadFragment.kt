c7a456d2bf6a2465b05f67e915f9e9a14f110bdf
PIYUJIN <yjyj020201@gmail.com>
2023-07-28 13:14:22 +0900

    [Feat] 게시글 보기 화면 toolBar 수정 기능 구현

--- app/src/main/java/com/test/board_project/PostReadFragment.kt
+++ app/src/main/java/com/test/board_project/PostReadFragment.kt
@@ -1,59 +1,77 @@
 package com.test.board_project
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.board_project.databinding.FragmentPostReadBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PostReadFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostReadFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var mainActivity: MainActivity
+    lateinit var fragmentPostReadBinding: FragmentPostReadBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_read, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostReadFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostReadFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        mainActivity = activity as MainActivity
+        fragmentPostReadBinding = FragmentPostReadBinding.inflate(inflater)
+
+        fragmentPostReadBinding.run{
+            toolbarPostRead.run{
+                title = "게시글 보기"
+
+                setNavigationIcon(R.drawable.arrow_back_24px)
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    navigationIcon?.colorFilter = BlendModeColorFilter(Color.DKGRAY, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.DKGRAY, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.JOIN_FRAGMENT)
+                }
+
+                inflateMenu(R.menu.menu_post_read)
+
+                setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.item_post_read_modify -> {
+                            if(!textInputEditTextPostReadSubject.isEnabled) {
+                                textInputEditTextPostReadSubject.isEnabled = true
+                                textInputEditTextPostReadContent.isEnabled = true
+                            } else {
+                                textInputEditTextPostReadSubject.isEnabled = false
+                                textInputEditTextPostReadContent.isEnabled = false
+                            }
+                        }
+                        R.id.item_post_read_delete -> {
+
+                        }
+                    }
+
+                    true
                 }
             }
+
+            textInputEditTextPostReadSubject.run{
+                setTextColor(Color.BLACK)
+            }
+
+            textInputEditTextPostReadContent.run{
+                setTextColor(Color.BLACK)
+            }
+        }
+
+        return fragmentPostReadBinding.root
     }
 }
