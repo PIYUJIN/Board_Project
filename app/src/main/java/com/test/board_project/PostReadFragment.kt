e8ff893d0b238ed35e2938996cdcd36c6d9c876e
PIYUJIN <yjyj020201@gmail.com>
2023-07-30 11:51:01 +0900

    [Feat] 게시글 수정 입력 오류 Dialog 기능 구현

--- app/src/main/java/com/test/board_project/PostReadFragment.kt
+++ app/src/main/java/com/test/board_project/PostReadFragment.kt
@@ -4,12 +4,16 @@ import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.opengl.Visibility
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout.END_ICON_CLEAR_TEXT
 import com.test.board_project.databinding.FragmentPostReadBinding
 
 class PostReadFragment : Fragment() {
@@ -26,6 +30,9 @@ class PostReadFragment : Fragment() {
         fragmentPostReadBinding = FragmentPostReadBinding.inflate(inflater)
 
         fragmentPostReadBinding.run{
+
+            buttonModifyPost.visibility = View.GONE
+
             toolbarPostRead.run{
                 title = "게시글 보기"
 
@@ -47,13 +54,7 @@ class PostReadFragment : Fragment() {
                 setOnMenuItemClickListener {
                     when(it.itemId) {
                         R.id.item_post_read_modify -> {
-                            if(!textInputEditTextPostReadSubject.isEnabled) {
-                                textInputEditTextPostReadSubject.isEnabled = true
-                                textInputEditTextPostReadContent.isEnabled = true
-                            } else {
-                                textInputEditTextPostReadSubject.isEnabled = false
-                                textInputEditTextPostReadContent.isEnabled = false
-                            }
+                            checkModify()
                         }
                         R.id.item_post_read_delete -> {
                             mainActivity.removeFragment(MainActivity.POST_READ_FRAGMENT)
@@ -72,8 +73,41 @@ class PostReadFragment : Fragment() {
             textInputEditTextPostReadContent.run{
                 setTextColor(Color.BLACK)
             }
+
+            buttonModifyPost.run {
+                setOnClickListener {
+                    checkModify()
+                }
+            }
         }
 
         return fragmentPostReadBinding.root
+    }
+
+    fun checkModify() {
+        fragmentPostReadBinding.run {
+            if(!textInputEditTextPostReadSubject.isEnabled) {
+                buttonModifyPost.visibility = View.VISIBLE
+                textInputEditTextPostReadSubject.isEnabled = true
+                textInputEditTextPostReadContent.isEnabled = true
+            } else {
+                if(textInputEditTextPostReadSubject.text.toString() == "" || textInputEditTextPostReadContent.text.toString() == "") {
+                    val builder = MaterialAlertDialogBuilder(mainActivity).apply {
+                        setTitle("게시글 수정 입력 오류")
+                        setMessage("게시글 제목이나 내용이 입력되어 있지 않습니다.")
+
+                        setPositiveButton("확인", null)
+                    }
+                    builder.show()
+                } else {
+                    val t1 = Toast.makeText(mainActivity, "게시글 수정 완료", Toast.LENGTH_LONG)
+                    t1.show()
+
+                    buttonModifyPost.visibility = View.GONE
+                    textInputEditTextPostReadSubject.isEnabled = false
+                    textInputEditTextPostReadContent.isEnabled = false
+                }
+            }
+        }
     }
 }
