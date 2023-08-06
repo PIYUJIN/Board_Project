8ebddecaffa0267dea727fcb5e443101eda2d350
PIYUJIN <yjyj020201@gmail.com>
2023-08-07 03:09:16 +0900

    [Feat] 사용자, 게시글 ViewModel 기능 구현

--- app/src/main/java/com/test/board_project/vm/UserViewModel.kt
+++ app/src/main/java/com/test/board_project/vm/UserViewModel.kt
@@ -1,4 +1,18 @@
 package com.test.board_project.vm
 
-class UserViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel() : ViewModel() {
+
+    var userId = MutableLiveData<String>()
+    var userPw = MutableLiveData<String>()
+    var userPwCheck = MutableLiveData<String>()
+
+    fun reset(){
+        userId.value = ""
+        userPw.value = ""
+        userPwCheck.value = ""
+    }
 }
+
