1aaf5889b26c2e354c5ead6b9f3da83bdf6c223c
PIYUJIN <yjyj020201@gmail.com>
2023-08-07 10:44:14 +0900

    [Fix] 주석 수정

--- app/src/main/java/com/test/board_project/vm/PostViewModel.kt
+++ app/src/main/java/com/test/board_project/vm/PostViewModel.kt
@@ -29,16 +29,16 @@ class PostViewModel() : ViewModel() {
     // 게시글 작성자 닉네임
     var postWriterNicknameList = MutableLiveData<MutableList<String>>()
 
-    init{
+    init {
         postDataList.value = mutableListOf<PostDataClass>()
         postWriterNicknameList.value = mutableListOf<String>()
     }
 
     // 게시글 읽기 화면
-    fun setPostReadData(postIdx:Double){
+    fun setPostReadData(postIdx:Double) {
         // 게시글 데이터를 가져온다
-        PostRepository.getPostInfo(postIdx){
-            for(c1 in it.result.children){
+        PostRepository.getPostInfo(postIdx) {
+            for(c1 in it.result.children) {
                 // 게시글 제목
                 postSubject.value = c1.child("postSubject").value as String
                 // 게시글 내용
@@ -56,7 +56,7 @@ class PostViewModel() : ViewModel() {
                     }
                 }
                 // 이미지가 있다면
-                if(postFileName.value != "None"){
+                if(postFileName.value != "None") {
                     PostRepository.getPostImage(postFileName.value!!) {
                         thread {
                             // 파일에 접근할 수 있는 경로를 이용해 URL 객체를 생성한다.
@@ -74,12 +74,12 @@ class PostViewModel() : ViewModel() {
     }
 
     // 게시글 목록
-    fun getPostAll(getPostType:Long){
+    fun getPostAll(getPostType:Long) {
         val tempList = mutableListOf<PostDataClass>()
         postWriterNicknameList.value = mutableListOf<String>()
 
         PostRepository.getPostAll {
-            for(c1 in it.result.children){
+            for(c1 in it.result.children) {
                 val postIdx = c1.child("postIdx").value as Long
                 val postImage = c1.child("postImage").value as String
                 val postSubject = c1.child("postSubject").value as String
@@ -88,23 +88,22 @@ class PostViewModel() : ViewModel() {
                 val postWriteDate = c1.child("postWriteDate").value as String
                 val postWriterIdx = c1.child("postWriterIdx").value as Long
 
-                if(getPostType != 0L && getPostType != postType){
+                if(getPostType != 0L && getPostType != postType) {
                     continue
                 }
 
                 val p1 = PostDataClass(postIdx, postType, postSubject, postText, postWriteDate, postImage, postWriterIdx)
                 tempList.add(p1)
 
-                UserRepository.getUserInfoByUserIdx(postWriterIdx){
+                UserRepository.getUserInfoByUserIdx(postWriterIdx) {
 
-                    for(c2 in it.result.children){
+                    for(c2 in it.result.children) {
                         val postWriterNickname = c2.child("userNickname").value as String
                         postWriterNicknameList.value?.add(postWriterNickname)
                     }
                 }
             }
-            // 데이터가 postIdx를 기준으로 오름 차순 정렬되어 있기 때문에
-            // 순서를 뒤집는다.
+            // 데이터가 postIdx를 기준으로 오름 차순 정렬 -> 순서 반대로 설정
             tempList.reverse()
 
             postDataList.value = tempList
