ffe80902fd616af51fe4966a3047c2e243344afc
PIYUJIN <yjyj020201@gmail.com>
2023-08-07 15:51:25 +0900

    [Feat] 게시글 수정, 삭제 기능 구현

--- app/src/main/java/com/test/board_project/PostReadFragment.kt
+++ app/src/main/java/com/test/board_project/PostReadFragment.kt
@@ -1,23 +1,42 @@
 package com.test.board_project
 
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
 import android.graphics.Color
+import android.graphics.ImageDecoder
+import android.graphics.Matrix
 import android.graphics.PorterDuff
+import android.media.ExifInterface
+import android.net.Uri
 import android.opengl.Visibility
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout.END_ICON_CLEAR_TEXT
 import com.test.board_project.databinding.FragmentPostReadBinding
+import com.test.board_project.repository.PostRepository
 import com.test.board_project.vm.PostViewModel
+import java.io.File
 
 class PostReadFragment : Fragment() {
 
@@ -28,6 +47,15 @@ class PostReadFragment : Fragment() {
 
     var readPostIdx = 0L
 
+    // 업로드할 이미지의 Uri
+    var uploadUri: Uri? = null
+
+    lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    lateinit var albumLauncher: ActivityResultLauncher<Intent>
+
+    // 새로운 이미지 설정 여부
+    var isSelectNewImage = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,6 +63,11 @@ class PostReadFragment : Fragment() {
 
         mainActivity = activity as MainActivity
         fragmentPostReadBinding = FragmentPostReadBinding.inflate(inflater)
+
+        // 카메라 설정
+        cameraLauncher = cameraSetting(fragmentPostReadBinding.imageViewPostRead)
+        // 앨범 설정
+        albumLauncher = albumSetting(fragmentPostReadBinding.imageViewPostRead)
 
         postViewModel = ViewModelProvider(mainActivity)[PostViewModel::class.java]
         postViewModel.run{
@@ -52,7 +85,8 @@ class PostReadFragment : Fragment() {
             }
             postFileName.observe(mainActivity) {
                 if(it == "None"){
-                    fragmentPostReadBinding.imageViewPostRead.visibility = View.GONE
+                    // 용량이 큰 이미지를 받아오기 전까지 보여줄 이미지 설정
+                    fragmentPostReadBinding.imageViewPostRead.setImageResource(R.mipmap.app_icon_foreground)
                 }
             }
             postImage.observe(mainActivity) {
@@ -61,7 +95,7 @@ class PostReadFragment : Fragment() {
             }
         }
 
-        fragmentPostReadBinding.run{
+        fragmentPostReadBinding.run {
 
             buttonModifyPost.visibility = View.GONE
 
@@ -91,8 +125,20 @@ class PostReadFragment : Fragment() {
 
                         }
                         R.id.item_post_read_delete -> {
-                            mainActivity.removeFragment(MainActivity.POST_READ_FRAGMENT)
-                            mainActivity.removeFragment(MainActivity.POST_WRITE_FRAGMENT)
+                            // 글 삭제
+                            PostRepository.removePost(readPostIdx) {
+                                // 이미지가 있는 경우 삭제
+                                if(postViewModel.postFileName.value != "None") {
+                                    PostRepository.removeImage(postViewModel.postFileName.value!!) {
+                                        mainActivity.removeFragment(MainActivity.POST_WRITE_FRAGMENT)
+                                        mainActivity.removeFragment(MainActivity.POST_READ_FRAGMENT)
+                                    }
+                                }
+                                else {
+                                    mainActivity.removeFragment(MainActivity.POST_READ_FRAGMENT)
+                                    mainActivity.removeFragment(MainActivity.POST_WRITE_FRAGMENT)
+                                }
+                            }
                         }
                     }
 
@@ -211,15 +257,153 @@ class PostReadFragment : Fragment() {
                     builder.show()
                 }
                 else {
-                    val t1 = Toast.makeText(mainActivity, "게시글 수정 완료", Toast.LENGTH_LONG)
-                    t1.show()
 
-                    toolbarPostRead.title = "게시글 보기"
-                    buttonModifyPost.visibility = View.GONE
-                    textInputEditTextPostReadSubject.isEnabled = false
-                    textInputEditTextPostReadContent.isEnabled = false
+                    val fileName = if(!isSelectNewImage) {
+                        "None"
+                    } else {
+                        if(postViewModel.postFileName.value == "None") {
+                            "image/img_${System.currentTimeMillis()}.jpg"
+                        } else {
+                            postViewModel.postFileName.value
+                        }
+                    }
+
+                    val postDataClass = PostDataClass(readPostIdx, 0, postSubject,
+                        postContent, "", fileName!!, mainActivity.loginUserClass.userIdx)
+
+                    PostRepository.modifyPost(postDataClass, isSelectNewImage) {
+                        if(isSelectNewImage) {
+                            PostRepository.uploadImage(uploadUri!!,fileName) {
+                                val t1 = Toast.makeText(mainActivity, "게시글 수정 완료", Toast.LENGTH_LONG)
+                                t1.show()
+
+                                toolbarPostRead.title = "게시글 보기"
+                                unregisterForContextMenu(imageViewPostRead)
+                                buttonModifyPost.visibility = View.GONE
+                                textInputEditTextPostReadSubject.isEnabled = false
+                                textInputEditTextPostReadContent.isEnabled = false
+                            }
+                        } else {
+                            val t1 = Toast.makeText(mainActivity, "게시글 수정 완료", Toast.LENGTH_LONG)
+                            t1.show()
+
+                            toolbarPostRead.title = "게시글 보기"
+                            unregisterForContextMenu(imageViewPostRead)
+                            buttonModifyPost.visibility = View.GONE
+                            textInputEditTextPostReadSubject.isEnabled = false
+                            textInputEditTextPostReadContent.isEnabled = false
+                        }
+                    }
                 }
             }
         }
+    }
+
+    // 카메라 관련 설정
+    fun cameraSetting(previewImageView: ImageView) : ActivityResultLauncher<Intent> {
+
+        // 사진 촬영을 위한 런처
+        val cameraContract = ActivityResultContracts.StartActivityForResult()
+        val cameraLauncher = registerForActivityResult(cameraContract) {
+            if(it?.resultCode == AppCompatActivity.RESULT_OK) {
+
+                // 새로운 이미지 선택 여부 설정
+                isSelectNewImage = true
+
+                // Uri를 이용해 이미지에 접근하여 Bitmap 객체 생성
+                val bitmap = BitmapFactory.decodeFile(uploadUri?.path)
+
+                // 이미지 크기 조정
+                val ratio = 1024.0 / bitmap.width
+                val targetHeight = (bitmap.height * ratio).toInt()
+                val bitmap2 = Bitmap.createScaledBitmap(bitmap, 1024, targetHeight, false)
+
+                // 회전 각도값
+                val degree = getDegree(uploadUri!!)
+
+                // 회전 이미지 생성
+                val matrix = Matrix()
+                matrix.postRotate(degree.toFloat())
+                val bitmap3 = Bitmap.createBitmap(bitmap2, 0, 0, bitmap2.width, bitmap2.height, matrix, false)
+                previewImageView.setImageBitmap(bitmap3)
+            }
+        }
+
+        return cameraLauncher
+    }
+
+    // 이미지 파일에 기록되어 있는 회전 정보를 가져온다.
+    fun getDegree(uri:Uri) : Int {
+
+        var exifInterface: ExifInterface? = null
+
+        // 사진 파일로 부터 tag 정보를 관리하는 객체를 추출한다.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            val photoUri = MediaStore.setRequireOriginal(uri)
+            // 스트림을 추출한다.
+            val inputStream = mainActivity.contentResolver.openInputStream(photoUri)
+            // ExifInterface 정보를 읽엉돈다.
+            exifInterface = ExifInterface(inputStream!!)
+        } else {
+            exifInterface = ExifInterface(uri.path!!)
+        }
+
+        var degree = 0
+        if(exifInterface != null){
+            // 각도 값을 가지고온다.
+            val orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1)
+
+            when(orientation){
+                ExifInterface.ORIENTATION_ROTATE_90 -> degree = 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> degree = 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> degree = 270
+            }
+        }
+        return degree
+    }
+
+    // 앨범 관련 설정
+    fun albumSetting(previewImageView: ImageView) : ActivityResultLauncher<Intent> {
+
+        val albumContract = ActivityResultContracts.StartActivityForResult()
+        val albumLauncher = registerForActivityResult(albumContract) {
+
+            if(it.resultCode == AppCompatActivity.RESULT_OK) {
+                // 선택한 이미지에 접근할 수 있는 Uri 객체를 추출한다.
+                if(it.data?.data != null){
+
+                    // 새로운 이미지 선택 여부 설정
+                    isSelectNewImage = true
+
+                    uploadUri = it.data?.data
+
+                    // 안드로이드 10 (Q) 이상이라면...
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                        // 이미지를 생성할 수 있는 디코더를 생성한다.
+                        val source = ImageDecoder.createSource(mainActivity.contentResolver, uploadUri!!)
+                        // Bitmap객체를 생성한다.
+                        val bitmap = ImageDecoder.decodeBitmap(source)
+
+                        previewImageView.setImageBitmap(bitmap)
+                    } else {
+                        // 컨텐츠 프로바이더를 통해 이미지 데이터 정보를 가져온다.
+                        val cursor = mainActivity.contentResolver.query(uploadUri!!, null, null, null, null)
+                        if(cursor != null){
+                            cursor.moveToNext()
+
+                            // 이미지의 경로를 가져온다.
+                            val idx = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                            val source = cursor.getString(idx)
+
+                            // 이미지를 생성하여 보여준다.
+                            val bitmap = BitmapFactory.decodeFile(source)
+                            previewImageView.setImageBitmap(bitmap)
+                        }
+                    }
+                }
+            }
+        }
+
+        return albumLauncher
     }
 }
